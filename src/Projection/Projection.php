80091eaf09e2f566b4497d1526846a13f976f82f
Thomas Tourlourat <thomas@tourlourat.com>
2017-09-26 15:30:23 +0200

    WIP

---
+++ src/Projection/Projection.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Thomas Tourlourat <thomas@tourlourat.com>
+ *
+ * Date: 26/09/2017
+ * Time: 15:18
+ */
+
+namespace DayUse\Istorija\Projection;
+
+
+use DayUse\Istorija\EventSourcing\DomainEvent\DomainEvent;
+use DayUse\Istorija\EventStore\EventMetadata;
+
+interface Projection
+{
+    public function apply(DomainEvent $event, EventMetadata $metadata);
+    public function reset();
+}
