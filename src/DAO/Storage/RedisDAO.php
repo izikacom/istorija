660956e2f94b754987010a16d1a0c63fd7536967
Thomas Tourlourat <thomas@tourlourat.com>
2017-11-14 14:38:21 +0100

    BC - redis serialization is now JSON

--- src/DAO/Storage/RedisDAO.php
+++ src/DAO/Storage/RedisDAO.php
@@ -46,7 +46,7 @@ class RedisDAO implements DAOInterface, BulkableInterface
     public function __construct(\Redis $redis, string $prefix)
     {
         Ensure::notBlank($prefix);
-        Ensure::eq(\Redis::SERIALIZER_PHP, $redis->getOption(\Redis::OPT_SERIALIZER), 'Are you sure that your redis serializer is well configured?');
+        Ensure::eq(\Redis::SERIALIZER_NONE, $redis->getOption(\Redis::OPT_SERIALIZER), 'Are you sure that your redis serializer is well configured?');
 
         $this->redis  = $redis;
         $this->prefix = $prefix;
@@ -58,13 +58,12 @@ class RedisDAO implements DAOInterface, BulkableInterface
     public function find(string $identifier)
     {
         $key  = $this->generateKey($identifier);
-        $data = $this->redis->get($key);
 
-        if (false === $data) {
+        if (false === $this->redis->exists($key)) {
             return null;
         }
 
-        return $data;
+        return $this->denormalize($this->redis->get($key));
     }
 
     /**
@@ -80,7 +79,10 @@ class RedisDAO implements DAOInterface, BulkableInterface
      */
     public function save(string $identifier, $data)
     {
-        $this->redis->set($this->generateKey($identifier), $data);
+        $this->redis->set(
+            $this->generateKey($identifier),
+            $this->normalize($data)
+        );
     }
 
     /**
@@ -120,5 +122,15 @@ class RedisDAO implements DAOInterface, BulkableInterface
     protected function generateKey(string $identifier)
     {
         return sprintf('%s:%s', $this->prefix, $identifier);
+    }
+
+    protected function normalize(array $data): string
+    {
+        return json_encode($data);
+    }
+
+    protected function denormalize(string $value): array
+    {
+        return json_decode($value, true);
     }
 }
