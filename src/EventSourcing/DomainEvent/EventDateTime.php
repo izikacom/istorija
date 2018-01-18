22e7e1486576b058de1048077b7d47e0d657cfed
Thomas Tourlourat <thomas@tourlourat.com>
2018-01-18 10:52:04 +0100

    Add EventDateTime implementation

---
+++ src/EventSourcing/DomainEvent/EventDateTime.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace DayUse\Istorija\EventSourcing\DomainEvent;
+
+class EventDateTime
+{
+    private const FORMAT_STRING = 'Y-m-d\TH:i:s.uP';
+
+    /**
+     * @var \DateTimeInterface
+     */
+    private static $mockedNow;
+
+    /**
+     * @var \DateTimeImmutable
+     */
+    private $dateTime;
+
+    private function __construct(\DateTimeImmutable $dateTime)
+    {
+        $this->dateTime = $dateTime;
+    }
+
+    public static function now(): self
+    {
+        if (static::$mockedNow) {
+            return new self(\DateTimeImmutable::createFromFormat(
+                self::FORMAT_STRING,
+                static::$mockedNow->format(self::FORMAT_STRING)
+            ));
+        }
+
+        return new self(
+            \DateTimeImmutable::createFromFormat(
+                'U.u',
+                sprintf('%.6F', microtime(true)),
+                new \DateTimeZone('UTC')
+            )
+        );
+    }
+
+    public function toDateTimeImmutable(): \DateTimeImmutable
+    {
+        return new \DateTimeImmutable((string) $this);
+    }
+
+    public function __toString(): string
+    {
+        return $this->dateTime->format(self::FORMAT_STRING);
+    }
+
+    public static function fromString(string $value): self
+    {
+        return new self(\DateTimeImmutable::createFromFormat(self::FORMAT_STRING, $value));
+    }
+
+    public function equals(self $dateTime): bool
+    {
+        return $this->__toString() === $dateTime->__toString();
+    }
+
+    public static function mockNow(\DateTimeInterface $dateTime): void
+    {
+        static::$mockedNow = $dateTime;
+    }
+}
---
+++ tests/EventSourcing/DomainEvent/EventDateTimeTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Thomas Tourlourat <thomas@tourlourat.com>
+ */
+
+namespace Dayuse\Test\Istorija\EventSourcing\DomainEvent;
+
+use DayUse\Istorija\EventSourcing\DomainEvent\EventDateTime;
+use PHPUnit\Framework\TestCase;
+
+class EventDateTimeTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function ensure_mock_now_is_possible()
+    {
+        $mockedNow = new \DateTimeImmutable('-3 days');
+
+        $this->assertNotEquals($mockedNow->getTimestamp(), EventDateTime::now()->toDateTimeImmutable()->getTimestamp());
+
+        EventDateTime::mockNow($mockedNow);
+
+        $this->assertEquals($mockedNow->getTimestamp(), EventDateTime::now()->toDateTimeImmutable()->getTimestamp());
+    }
+
+    /**
+     * @test
+     */
+    public function ensure_to_string_is_working()
+    {
+        $this->assertNotEmpty((string)EventDateTime::now());
+    }
+
+    /**
+     * @test
+     */
+    public function ensure_from_string_is_working()
+    {
+        $this->assertNotEmpty(EventDateTime::fromString('2017-10-21T09:24:12.481962+00:00'));
+        $this->assertNotEmpty(EventDateTime::fromString('2017-10-24T09:08:35.000000+00:00'));
+    }
+}
