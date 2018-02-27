9f9bb1a23125a25de93b13cd414d241911be549e
Thomas Tourlourat <thomas@tourlourat.com>
2018-02-27 15:20:38 +0100

    Store command with failure reason to CommandNotValidException

--- src/CommandBus/Validator/Exception/CommandNotValidException.php
+++ src/CommandBus/Validator/Exception/CommandNotValidException.php
@@ -8,6 +8,42 @@
 
 namespace Dayuse\Istorija\CommandBus\Validator\Exception;
 
+use Dayuse\Istorija\CommandBus\Command;
 use Dayuse\Istorija\Exception;
+use Verraes\ClassFunctions\ClassFunctions;
 
-class CommandNotValidException extends \RuntimeException implements Exception {}
+class CommandNotValidException extends \RuntimeException implements Exception
+{
+    /** @var Command */
+    private $command;
+
+    /** @var string */
+    private $reason;
+
+    public function __construct(Command $command, string $reason = null)
+    {
+        $this->command = $command;
+        $this->reason  = $reason;
+
+        if($this->reason) {
+            parent::__construct(sprintf('%s could not be handled because of %s',
+                ClassFunctions::fqcn($command),
+                $this->reason
+            ));
+        }
+
+        parent::__construct(sprintf('%s could not be handled.',
+            ClassFunctions::fqcn($command)
+        ));
+    }
+
+    public function getCommand(): Command
+    {
+        return $this->command;
+    }
+
+    public function getReason(): string
+    {
+        return $this->reason;
+    }
+}
