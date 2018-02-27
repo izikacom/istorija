c2c3e4ed1228c9b2fca9f7a914cfb270200e80dd
Thomas Tourlourat <thomas@tourlourat.com>
2018-02-27 16:09:54 +0100

    Use only one call to parent::__constructor and take care of previous exception

--- src/CommandBus/Validator/Exception/CommandNotValidException.php
+++ src/CommandBus/Validator/Exception/CommandNotValidException.php
@@ -20,21 +20,24 @@ class CommandNotValidException extends \RuntimeException implements Exception
     /** @var string */
     private $reason;
 
-    public function __construct(Command $command, string $reason = null)
+    public function __construct(Command $command, string $reason = null, \Throwable $previous = null)
     {
         $this->command = $command;
         $this->reason  = $reason;
 
         if($this->reason) {
-            parent::__construct(sprintf('%s could not be handled because of %s',
+            $message = sprintf('%s could not be handled because of %s',
                 ClassFunctions::fqcn($command),
                 $this->reason
-            ));
+            );
+        }
+        else {
+            $message = sprintf('%s could not be handled.',
+                ClassFunctions::fqcn($command)
+            );
         }
 
-        parent::__construct(sprintf('%s could not be handled.',
-            ClassFunctions::fqcn($command)
-        ));
+        parent::__construct($message, 0, $previous);
     }
 
     public function getCommand(): Command
