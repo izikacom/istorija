fb8268fbe74d13180b30f467452e47c95eded26e
Thomas Tourlourat <thomas@tourlourat.com>
2018-05-18 11:48:17 +0200

    Add additionalMetadata to on the eventStore event envelope factory & use class instead of type metadata

--- src/EventSourcing/EventEnvelopeFactory.php
+++ src/EventSourcing/EventEnvelopeFactory.php
@@ -1,16 +1,9 @@
 <?php
-/**
- * @author Thomas Tourlourat <thomas@tourlourat.com>
- *
- * Date: 17/10/2017
- * Time: 16:40
- */
-
 namespace Dayuse\Istorija\EventSourcing;
 
 use Dayuse\Istorija\EventSourcing\DomainEvent\DomainEventCollection;
 
 interface EventEnvelopeFactory
 {
-    public function fromDomainEvents(DomainEventCollection $domainEvents): array;
+    public function fromDomainEvents(DomainEventCollection $domainEvents, array $additionalMetadata = []): array;
 }
